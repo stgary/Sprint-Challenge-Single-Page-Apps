ee989001d5871ea69b8eb6cf8ad0d8590b32bb45
Stephen Gary <stgary0@gmail.com>
2020-02-28 11:51:02 -0500

    CharacterList

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,48 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    Axios.get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      console.log(response.data.results);
+      const characters = response.data.results.filter(character => 
+        character.name.toLowerCase().includes(query.toLowerCase()
+        ));
+        setData(characters);
+    })
+  }, [query]);
+
+  const handleInputChange = e => {
+    setQuery(e.target.value);
+  }
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <form>
+        <input 
+          type="text"
+          name="name"
+          placeholder="search by name"
+          value={query}
+          onChange={handleInputChange}
+        />
+      </form>
+      <div>
+        {data.map(data => {
+          return (
+            <div key={data.id}>
+              <h2>{data.name}</h2>
+              <h3>Species: {data.species}</h3>
+              <h3>Status: {data.status}</h3>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 }
