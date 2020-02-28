b3af26a7174c99936445b1ed2bed8be14011d97c
Stephen Gary <stgary0@gmail.com>
2020-02-28 11:25:59 -0500

    Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage.js";
+import CharacterList from "./components/CharacterList.js";
+import { Link, Switch, Route } from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
       <Header />
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
+      <Route exact path="/character-list">
+        <CharacterList />
+      </Route>
     </main>
   );
 }
